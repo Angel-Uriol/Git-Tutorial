69f1248fb71544d14056cc814f29eb1711aa2562
Angel Uriol <angeluriol@Angels-MacBook-Air-3.local>
2024-04-06 19:24:56 -0600

    version 1

---
+++ scratch.py
@@ -0,0 +1,24 @@
+import random
+
+def solution(password, key):
+    table = {}
+    counter = 0
+    for i in key:
+        first_value = ord('a') + counter
+        table[first_value] = ord(i)
+        counter+=1
+    return password.translate(table)
+
+def solution2(password, key):
+    table = {ord('a') + i : ord(k) for i, k in enumerate(key)}
+    return password.translate(table)
+
+
+def generate_random_list(n):
+    result = list(range(1, n + 1))  # Generate a list of numbers from 1 to n
+    random.shuffle(result)  # Shuffle the list
+    return result
+
+
+print(generate_random_list(100))
+
